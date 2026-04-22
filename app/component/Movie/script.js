1d6e09f9c84aa4e82d4822a81630b78ec643d325
Mamedov Alik <mamedov1@mmi-dev.unilim.fr>
2026-04-22 17:11:08 +0200

    “update”

---
+++ app/component/Movie/script.js
@@ -0,0 +1,33 @@
+
+let templateFile = await fetch("./component/Movie/template.html");
+let template = await templateFile.text();
+
+let Movie = {};
+
+
+Movie.format = function (movies) {
+
+    if (!movies || movies.length === 0) {
+        return `<p class="no-movies">Aucun film disponible pour le moment.</p>`;
+    }
+
+    let htmlFinal = '<div class="movie-list">';
+
+
+    movies.forEach(movie => {
+        let card = template;
+        card = card.replace(/{{title}}/g, movie.title);
+        card = card.replace("{{poster}}", movie.poster);
+        card = card.replace("{{year}}", movie.year || "");
+        card = card.replace("{{description}}", movie.description);
+        card = card.replace("{{director}}", movie.director);
+        card = card.replace("{{trailer}}", movie.trailer);
+        
+        htmlFinal += card;
+    });
+
+    htmlFinal += '</div>';
+    return htmlFinal;
+};
+
+export { Movie };
